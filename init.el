41288c35e2e15e29cd4b7fd796fb2c290241aad6
Daw-Ran Liou <dawran6@gmail.com>
2020-04-03 21:33:38 -0700

    Setup use-package and evil

---
+++ init.el
@@ -0,0 +1,23 @@
+(setq inhibit-startup-message t)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(tool-bar-mode -1)
+
+(use-package evil
+  :ensure t
+  :custom
+  (evil-move-beyond-eol t)
+  :init
+  (evil-mode t))
+
