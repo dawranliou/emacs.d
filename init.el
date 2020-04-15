a4b05c57dece5327dfd069b8fd7aa230619d0b86
Daw-Ran Liou <dawran6@gmail.com>
2020-04-14 17:37:00 -0700

    Activate web mode for scss

--- init.el
+++ init.el
@@ -458,6 +458,7 @@
 (use-package web-mode
   :config
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.scss?\\'" . web-mode))
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
