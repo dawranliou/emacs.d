5f9ce8e140bbb65a09b36da4735fa0a8db4c9d5e
Daw-Ran Liou <dawran6@gmail.com>
2020-05-09 10:08:03 -0700

    Enhance evil-multiedit

--- init.el
+++ init.el
@@ -291,26 +291,16 @@
 
 (use-package evil-multiedit
   :config
-  ;; Highlights all matches of the selection in the buffer.
   (define-key evil-visual-state-map "R" 'evil-multiedit-match-all)
-
-  ;; Match the word under cursor (i.e. make it an edit region). Consecutive presses will
-  ;; incrementally add the next unmatched match.
-  (define-key evil-normal-state-map (kbd "M-d") 'evil-multiedit-match-and-next)
-  ;; Match selected region.
+  (define-key evil-normal-state-map (kbd "M-d") 'evil-multiedit-match-symbol-and-next)
+  (define-key evil-normal-state-map (kbd "M-D") 'evil-multiedit-match-symbol-and-prev)
   (define-key evil-visual-state-map (kbd "M-d") 'evil-multiedit-match-and-next)
-  ;; Insert marker at point
+  (define-key evil-visual-state-map (kbd "M-D") 'evil-multiedit-match-and-prev)
   (define-key evil-insert-state-map (kbd "M-d") 'evil-multiedit-toggle-marker-here)
 
-  ;; Same as M-d but in reverse.
-  (define-key evil-normal-state-map (kbd "M-D") 'evil-multiedit-match-and-prev)
-  (define-key evil-visual-state-map (kbd "M-D") 'evil-multiedit-match-and-prev)
-
-  ;; OPTIONAL: If you prefer to grab symbols rather than words, use
-  ;; `evil-multiedit-match-symbol-and-next` (or prev).
-
   ;; Restore the last group of multiedit regions.
-  (define-key evil-visual-state-map (kbd "C-M-D") 'evil-multiedit-restore)
+  (define-key evil-normal-state-map (kbd "C-M-d") 'evil-multiedit-restore)
+  (define-key evil-visual-state-map (kbd "C-M-d") 'evil-multiedit-restore)
 
   ;; RET will toggle the region under the cursor
   (define-key evil-multiedit-state-map (kbd "RET") 'evil-multiedit-toggle-or-restrict-region)
