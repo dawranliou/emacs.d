3fcbd2c3d8324308dfefc59a884a0503088b2a9b
Daw-Ran Liou <dawran.liou@kirasystems.com>
2020-10-28 06:24:23 +0800

    Tweak text scale hydra

--- init.el
+++ init.el
@@ -141,8 +141,8 @@
 
 (defhydra hydra-text-scale (:timeout 4)
   "scale text"
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
+  ("j" text-scale-increase "+")
+  ("k" text-scale-decrease "-")
   ("f" nil "finished" :exit t))
 
 (dawran/leader-keys
