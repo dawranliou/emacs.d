a2f4d4870f8ff4ea57e662c1124881ba740e90c6
Daw-Ran Liou <dawran.liou@kirasystems.com>
2020-10-28 05:07:40 +0800

    Add helpful help commands

--- init.el
+++ init.el
@@ -95,6 +95,16 @@
   :config
   (counsel-mode 1))
 
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
