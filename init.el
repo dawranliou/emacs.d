71d19e8b11216c289d408ba65cd01050e83d65a6
Daw-Ran Liou <dawran.liou@kirasystems.com>
2020-10-31 04:12:11 +0800

    Add vterm and eshell

--- init.el
+++ init.el
@@ -391,6 +391,30 @@
 
 (setq exec-path (append exec-path '("/usr/local/bin")))
 
+(use-package vterm
+  :commands vterm
+  :config
+  (setq vterm-max-scrollback 10000))
+
+(defun dawran/configure-eshell ()
+  ;; Save command history when commands are entered
+  (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
+
+  ;; Truncate buffer for performance
+  (add-to-list 'eshell-output-filter-functions 'eshell-truncate-buffer)
+
+  ;; Bind some useful keys for evil-mode
+  (evil-define-key '(normal insert visual) eshell-mode-map (kbd "C-r") 'counsel-esh-history)
+  (evil-define-key '(normal insert visual) eshell-mode-map (kbd "C-a") 'eshell-bol)
+
+  (setq eshell-history-size          10000
+        eshell-buffer-maximum-lines  10000
+        eshell-hist-ignoredups           t
+        eshell-scroll-to-bottom-on-input t))
+
+(use-package eshell
+  :hook (eshell-first-time-mode . dawran/configure-eshell))
+
 (use-package company
   :after lsp-mode
   :hook (lsp-mode . company-mode)
