777b920938e59720b874755bcfacf97ca39e6373
Daw-Ran Liou <dawran.liou@kirasystems.com>
2020-10-30 09:08:15 +0800

    Fix undo-fu mode

--- init.el
+++ init.el
@@ -406,7 +406,6 @@
 
 (use-package undo-fu
   :config
-  (global-undo-tree-mode -1)
   (define-key evil-normal-state-map "u" 'undo-fu-only-undo)
   (define-key evil-normal-state-map "\C-r" 'undo-fu-only-redo))
 
