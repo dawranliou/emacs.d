87260155aa101cde49bc7721f546fca660afecd8
Daw-Ran Liou <dawran.liou@kirasystems.com>
2020-11-23 07:21:49 +0800

    Move company mode

--- init.el
+++ init.el
@@ -569,38 +569,6 @@
 (dawran/leader-keys
   "e" 'eshell)
 
-(use-package company
-  :after lsp-mode
-  :hook (lsp-mode . company-mode)
-  :bind (:map company-active-map
-         ("<tab>" . company-complete-selection))
-        (:map lsp-mode-map
-         ("<tab>" . company-indent-or-complete-common))
-  :custom
-  (company-minimum-prefix-length 1)
-  (company-idle-delay nil)
-  :config
-  (add-hook 'evil-local-mode-hook
-            (lambda ()
-              ;; Note:
-              ;; Check if `company-emulation-alist' is in
-              ;; `emulation-mode-map-alists', if true, call
-              ;; `company-ensure-emulation-alist' to ensure
-              ;; `company-emulation-alist' is the first item of
-              ;; `emulation-mode-map-alists', thus has a higher
-              ;; priority than keymaps of evil-mode.
-              ;; We raise the priority of company-mode keymaps
-              ;; unconditionally even when completion is not
-              ;; activated. This should not cause problems,
-              ;; because when completion is activated, the value of
-              ;; `company-emulation-alist' is ((t . company-my-keymap)),
-              ;; when completion is not activated, the value is ((t . nil)).
-              (when (memq 'company-emulation-alist emulation-mode-map-alists)
-                (company-ensure-emulation-alist)))))
-
-(use-package company-box
-  :hook (company-mode . company-box-mode))
-
 (use-package projectile
   :diminish projectile-mode
   :config (projectile-mode)
@@ -775,6 +743,38 @@
     (dawran/set-markdown-header-font-sizes))
 
   (add-hook 'markdown-mode-hook 'dw/markdown-mode-hook)))
+
+(use-package company
+  :hook (;(lsp-mode . company-mode)
+         (eglot-mode . company-mode))
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map eglot-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay nil)
+  :config
+  (add-hook 'evil-local-mode-hook
+            (lambda ()
+              ;; Note:
+              ;; Check if `company-emulation-alist' is in
+              ;; `emulation-mode-map-alists', if true, call
+              ;; `company-ensure-emulation-alist' to ensure
+              ;; `company-emulation-alist' is the first item of
+              ;; `emulation-mode-map-alists', thus has a higher
+              ;; priority than keymaps of evil-mode.
+              ;; We raise the priority of company-mode keymaps
+              ;; unconditionally even when completion is not
+              ;; activated. This should not cause problems,
+              ;; because when completion is activated, the value of
+              ;; `company-emulation-alist' is ((t . company-my-keymap)),
+              ;; when completion is not activated, the value is ((t . nil)).
+              (when (memq 'company-emulation-alist emulation-mode-map-alists)
+                (company-ensure-emulation-alist)))))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
 
 (use-package flycheck
   :defer t
