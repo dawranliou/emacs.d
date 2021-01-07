dfec4730c352ebabf040d6e40de1f0b16bf16f34
Daw-Ran Liou <hi@dawranliou.com>
2021-01-07 21:11:02 +0800

    Add suggested Embark configurations

--- init.el
+++ init.el
@@ -347,9 +347,35 @@
 
 (use-package embark
   :bind
-  (:map minibuffer-local-map
-        ("C-S-a" . embark-act)
-        ("C-c C-o" . embark-occur)))
+  ("C-S-a" . embark-act)
+
+  :config
+  ;; For Selectrum users:
+  (defun current-candidate+category ()
+    (when selectrum-active-p
+      (cons (selectrum--get-meta 'category)
+            (selectrum-get-current-candidate))))
+
+  (add-hook 'embark-target-finders #'current-candidate+category)
+
+  (defun current-candidates+category ()
+    (when selectrum-active-p
+      (cons (selectrum--get-meta 'category)
+            (selectrum-get-current-candidates
+             ;; Pass relative file names for dired.
+             minibuffer-completing-file-name))))
+
+  (add-hook 'embark-candidate-collectors #'current-candidates+category)
+
+  ;; No unnecessary computation delay after injection.
+  (add-hook 'embark-setup-hook 'selectrum-set-selected-candidate)
+
+  :custom
+  (embark-action-indicator
+   (lambda (map)
+     (which-key--show-keymap "Embark" map nil nil 'no-paging)
+     #'which-key--hide-popup-ignore-command)
+   embark-become-indicator embark-action-indicator))
 
 (use-package consult
   ;; Replace bindings. Lazily loaded due by `use-package'.
