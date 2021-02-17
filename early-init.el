8064bafe61798404836ae170628805d3c836c707
Daw-Ran Liou <hi@dawranliou.com>
2021-02-17 10:49:31 +0800

    Fix default-frame-alist

--- early-init.el
+++ early-init.el
@@ -7,5 +7,5 @@
 (setq package-enable-at-startup nil)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
+(push '(vertical-scroll-bars . nil) default-frame-alist)
 (setq frame-inhibit-implied-resize t)
