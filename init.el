5c2a097c659200f75b8bef0e6e422931c3934779
Daw-Ran Liou <hi@dawranliou.com>
2021-03-30 22:54:33 +0800

    Cleanup embark setup

--- init.el
+++ init.el
@@ -482,36 +482,15 @@
 (use-package embark
   :bind
   (("C-S-a" . embark-act)
+   ("C-h B" . embark-bindings)
    :map minibuffer-local-map
    ("C-d" . embark-act))
-
-  :config
-  ;; For Selectrum users:
-  (defun current-candidate+category ()
-    (when selectrum-active-p
-      (cons (selectrum--get-meta 'category)
-            (selectrum-get-current-candidate))))
-
-  (add-hook 'embark-target-finders #'current-candidate+category)
-
-  (defun current-candidates+category ()
-    (when selectrum-active-p
-      (cons (selectrum--get-meta 'category)
-            (selectrum-get-current-candidates
-             ;; Pass relative file names for dired.
-             minibuffer-completing-file-name))))
-
-  (add-hook 'embark-candidate-collectors #'current-candidates+category)
-
-  ;; No unnecessary computation delay after injection.
-  (add-hook 'embark-setup-hook 'selectrum-set-selected-candidate)
-
-  :custom
-  (embark-action-indicator
-   (lambda (map)
-     (which-key--show-keymap "Embark" map nil nil 'no-paging)
-     #'which-key--hide-popup-ignore-command)
-   embark-become-indicator embark-action-indicator))
+  :init
+  (setq embark-action-indicator
+        (lambda (map)
+          (which-key--show-keymap "Embark" map nil nil 'no-paging)
+          #'which-key--hide-popup-ignore-command)
+        embark-become-indicator embark-action-indicator))
 
 (use-package helpful
   :bind (;; Remap standard commands.
