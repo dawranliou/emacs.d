2dd8da6b6e0123ed5652d3e774db3a7449171978
Daw-Ran Liou <hi@dawranliou.com>
2021-04-09 07:36:42 -0700

    Configure hippie-exp

--- init.el
+++ init.el
@@ -418,6 +418,21 @@
 (use-package hide-mode-line
   :commands hide-mode-line-mode)
 
+(use-package hippie-exp
+  :straight nil
+  :custom
+  (hippie-expand-try-functions-list
+   '(try-complete-file-name-partially
+     try-complete-file-name
+     try-expand-all-abbrevs
+     ;; try-expand-list
+     ;; try-expand-line
+     try-expand-dabbrev
+     try-expand-dabbrev-all-buffers
+     try-expand-dabbrev-from-kill
+     try-complete-lisp-symbol-partially
+     try-complete-lisp-symbol)))
+
 (use-package orderless
   :custom
   (completion-styles '(orderless))
