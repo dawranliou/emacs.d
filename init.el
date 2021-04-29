f460e779b5d864d0e9e3f5701623cde70efadffa
Daw-Ran Liou <hi@dawranliou.com>
2021-04-28 21:52:27 -0700

    Simple mode line

--- init.el
+++ init.el
@@ -323,6 +323,22 @@
 (use-package display-fill-column-indicator
   :hook (prog-mode . display-fill-column-indicator-mode))
 
+(setq-default mode-line-format
+              '("%e"
+                mode-line-front-space
+                mode-line-mule-info
+                mode-line-client
+                mode-line-modified
+                mode-line-remote
+                mode-line-frame-identification
+                mode-line-buffer-identification
+                "   "
+                mode-line-position
+                evil-mode-line-tag
+                ;; (vc-mode vc-mode)
+                "  "
+                mode-line-modes mode-line-misc-info mode-line-end-spaces))
+
 (defun dawran/visual-fill ()
   (setq visual-fill-column-width 100
         visual-fill-column-center-text t)
