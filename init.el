f24083866d8e0390c7eb0162c5f85afa77029e03
Daw-Ran Liou <hi@dawranliou.com>
2021-08-13 23:12:20 -0700

    Simplify installation with straight.el

--- init.el
+++ init.el
@@ -285,18 +285,9 @@ used to create a new scratch buffer."
       ;; think.
       straight-check-for-modifications '(check-on-save find-when-checking))
 
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(load (expand-file-name
+       "straight/repos/straight.el/bootstrap.el" user-emacs-directory)
+      nil 'nomessage)
 
 (straight-use-package 'use-package)
 ;; (setq use-package-verbose t)
