0c71c5081b5bb0b0ef64f2476cd0f5ebde757089
Daw-Ran Liou <hi@dawranliou.com>
2021-12-28 10:38:12 -0800

    Remove function eshell-here

--- init.el
+++ init.el
@@ -331,23 +331,6 @@ scratch buffer.  The newly created buffer would have the
   (interactive)
   (kill-line 0))
 
-(defun eshell-here ()
-  "Opens up a new shell in the directory associated with the
-current buffer's file. The eshell is renamed to match that
-directory to make multiple eshell windows easier."
-  (interactive)
-  (let* ((parent (if (buffer-file-name)
-                     (file-name-directory (buffer-file-name))
-                   default-directory))
-         (height (/ (window-total-height) 3))
-         (name   (car (last (split-string parent "/" t)))))
-    (split-window-vertically (- height))
-    (other-window 1)
-    (eshell "new")
-    (rename-buffer (concat "*eshell: " name "*"))
-    (insert (concat "ls"))
-    (eshell-send-input)))
-
 (defun backward-kill-word-or-region (&optional arg)
   "Kill word backwards unless region is active,
 kill region instead"
