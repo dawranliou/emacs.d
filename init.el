73c1d80523d17638625e3bce8d1a5ee413e595d5
Daw-Ran Liou <hi@dawranliou.com>
2022-04-25 10:31:44 -0700

    Enable set-goal-column

--- init.el
+++ init.el
@@ -143,6 +143,7 @@
 (put 'dired-find-alternate-file 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+(put 'set-goal-column 'disabled nil)
 
 ;;; Keybindings
 
