93b25b48bc452cb3a17b43c306695a34e988d899
Daw-Ran Liou <hi@dawranliou.com>
2023-03-20 06:55:22 -0700

    Update packages 2023-03-20

---
+++ elpa/rg-20230318.1152/rg-pkg.el
@@ -0,0 +1,15 @@
+(define-package "rg" "20230318.1152" "A search tool based on ripgrep"
+  '((emacs "25.1")
+    (transient "0.3.0")
+    (wgrep "2.1.10"))
+  :commit "766e2991fc589bdaa9b610b9a0b76e3c78ada37f" :authors
+  '(("David Landell" . "david.landell@sunnyhill.email")
+    ("Roland McGrath" . "roland@gnu.org"))
+  :maintainer
+  '("David Landell" . "david.landell@sunnyhill.email")
+  :keywords
+  '("matching" "tools")
+  :url "https://github.com/dajva/rg.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
