5edf97c1cd28aaf4ddaf19ee6060b9669b5fa860
Daw-Ran Liou <hi@dawranliou.com>
2023-04-25 06:51:46 -0700

    Sync packages 2023-04-25

---
+++ elpa/fennel-mode-20230423.1916/fennel-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "fennel-mode" "20230423.1916" "A major-mode for editing Fennel code"
+  '((emacs "26.1"))
+  :commit "94c1af291f44be00f08ff2cb0cc39e6f3edf8076" :authors
+  '(("Phil Hagelberg"))
+  :maintainer
+  '("Phil Hagelberg")
+  :keywords
+  '("languages" "tools")
+  :url "https://git.sr.ht/~technomancy/fennel-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/sly-20230411.1523/sly-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "sly" "20230411.1523" "Sylvester the Cat's Common Lisp IDE"
-  '((emacs "24.3"))
-  :commit "149d7ef7844b3824b1df148295cedf6ff85c2b99" :keywords
-  '("languages" "lisp" "sly")
-  :url "https://github.com/joaotavora/sly")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
