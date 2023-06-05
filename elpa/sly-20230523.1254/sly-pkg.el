3409d4ab63d04da77ca0d50ae7c29d1066f0f953
Daw-Ran Liou <hi@dawranliou.com>
2023-06-05 07:29:33 -0700

    Sync packages 2023-06-05

---
+++ elpa/sly-20230523.1254/sly-pkg.el
@@ -0,0 +1,8 @@
+(define-package "sly" "20230523.1254" "Sylvester the Cat's Common Lisp IDE"
+  '((emacs "24.3"))
+  :commit "e6bc13d9f55e3763e6daea527cb88a776c479d8e" :keywords
+  '("languages" "lisp" "sly")
+  :url "https://github.com/joaotavora/sly")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
