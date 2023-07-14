7881a5109ae09525d12c2d5ec1e2d8352c01543e
Daw-Ran Liou <hi@dawranliou.com>
2023-07-14 14:43:34 -0700

    Sync packages 2023-07-14

---
+++ elpa/jinx-20230711.1551/jinx-pkg.el
@@ -0,0 +1,15 @@
+(define-package "jinx" "20230711.1551" "Enchanted Spell Checker"
+  '((emacs "27.1")
+    (compat "29.1.4.0"))
+  :commit "c9fbfd40259e59a2c0e4ad630611d8f88d040cc6" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("convenience" "wp")
+  :url "https://github.com/minad/jinx")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/markdown-mode-20230714.440/markdown-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "markdown-mode" "20230714.440" "Major mode for Markdown-formatted text"
+  '((emacs "26.1"))
+  :commit "8dc04cc6762e1863474eb60739a31e45df487f14" :authors
+  '(("Jason R. Blevins" . "jblevins@xbeta.org"))
+  :maintainers
+  '(("Jason R. Blevins" . "jblevins@xbeta.org"))
+  :maintainer
+  '("Jason R. Blevins" . "jblevins@xbeta.org")
+  :keywords
+  '("markdown" "github flavored markdown" "itex")
+  :url "https://jblevins.org/projects/markdown-mode/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
