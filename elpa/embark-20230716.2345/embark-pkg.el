41273f90e7f0cbeaee6647afb35e919941d3aa06
Daw-Ran Liou <hi@dawranliou.com>
2023-07-28 14:40:42 -0700

    Sync packages 2023-07-28

---
+++ elpa/embark-20230716.2345/embark-pkg.el
@@ -0,0 +1,15 @@
+(define-package "embark" "20230716.2345" "Conveniently act on minibuffer completions"
+  '((emacs "27.1")
+    (compat "29.1.4.0"))
+  :commit "9a44418c349e41020cdc5ad1bd21e8c77a429062" :authors
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainers
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainer
+  '("Omar Antolín Camarena" . "omar@matem.unam.mx")
+  :keywords
+  '("convenience")
+  :url "https://github.com/oantolin/embark")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
