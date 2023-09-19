dedeb3f49b275014ce597bff9e61970349bbfb71
Daw-Ran Liou <hi@dawranliou.com>
2023-09-19 09:30:31 +0800

    Give elisp-shorthand-font-lock-face better contrasts

--- init.el
+++ init.el
@@ -296,6 +296,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(eglot-highlight-symbol-face ((t (:inherit lazy-highlight))))
+ '(elisp-shorthand-font-lock-face ((t (:inherit font-lock-keyword-face :weight bold))))
  '(fixed-pitch ((t (:family "Iosevka" :height 150))))
  '(variable-pitch ((t (:family "Sans Serif" :height 170)))))
 
