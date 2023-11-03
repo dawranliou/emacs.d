1131299448403a16bb45e218610c8f674e976818
Daw-Ran Liou <hi@dawranliou.com>
2023-11-03 14:14:38 -0700

    Sync packages 2023-11-03

---
+++ elpa/cape-20231029.1004/cape-pkg.el
@@ -0,0 +1,15 @@
+(define-package "cape" "20231029.1004" "Completion At Point Extensions"
+  '((emacs "27.1")
+    (compat "29.1.4.2"))
+  :commit "085287d793ea2fd06b47ebcb6ab08e88533e5185" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("abbrev" "convenience" "matching" "completion" "wp")
+  :url "https://github.com/minad/cape")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
