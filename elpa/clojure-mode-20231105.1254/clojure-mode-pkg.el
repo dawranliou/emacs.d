7d204f4d688672fe0480bf90af942c705f5bdc70
Daw-Ran Liou <hi@dawranliou.com>
2023-11-09 15:07:48 -0800

    Sync packages 2023-11-09

---
+++ elpa/clojure-mode-20231105.1254/clojure-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "clojure-mode" "20231105.1254" "Major mode for Clojure code"
+  '((emacs "25.1"))
+  :commit "481ca480e8b7b6c90881f8bd8434addab1d33778" :maintainers
+  '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
+  :maintainer
+  '("Bozhidar Batsov" . "bozhidar@batsov.dev")
+  :keywords
+  '("languages" "clojure" "clojurescript" "lisp")
+  :url "https://github.com/clojure-emacs/clojure-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/orderless-20231107.2103/orderless-pkg.el
@@ -0,0 +1,14 @@
+(define-package "orderless" "20231107.2103" "Completion style for matching regexps in any order"
+  '((emacs "26.1"))
+  :commit "b38a8e85d605c1018e4c8ba7beacb1ec6708a810" :authors
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainers
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainer
+  '("Omar Antolín Camarena" . "omar@matem.unam.mx")
+  :keywords
+  '("extensions")
+  :url "https://github.com/oantolin/orderless")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
