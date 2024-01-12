79d62f5ab953aae0dfeeeb08fb5eceba7cd8028d
Daw-Ran Liou <hi@dawranliou.com>
2024-01-12 12:53:10 -0800

    Sync packages 2024-01-12

---
+++ elpa/jinx-20240109.1806/jinx-pkg.el
@@ -0,0 +1,15 @@
+(define-package "jinx" "20240109.1806" "Enchanted Spell Checker"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "03efb689ca134127c186b8da5a2a91b04889ea2b" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("convenience" "text")
+  :url "https://github.com/minad/jinx")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/vertico-20240103.1359/vertico-pkg.el
@@ -0,0 +1,15 @@
+(define-package "vertico" "20240103.1359" "VERTical Interactive COmpletion"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "357c2503a7787193d50dafd8212f2b1cc165ae16" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("convenience" "files" "matching" "completion")
+  :url "https://github.com/minad/vertico")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
