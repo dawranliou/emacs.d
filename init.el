367e34358eace9f60d52db2859063f446524a1ab
Daw-Ran Liou <hi@dawranliou.com>
2024-01-27 01:12:47 +0800

    Don't use org-indent-mode

--- init.el
+++ init.el
@@ -760,7 +760,6 @@ reuse it's window, otherwise create new one."
                    (,electric-pair-inhibit-predicate c)))))
 
 (add-hook 'org-mode-hook #'org-mode-setup)
-(add-hook 'org-mode-hook #'org-indent-mode)
 (add-hook 'org-mode-hook #'variable-pitch-mode)
 (add-hook 'org-mode-hook #'auto-fill-mode)
 (add-hook 'org-mode-hook #'toggle-truncate-lines)
