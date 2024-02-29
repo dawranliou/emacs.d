fecbff07f6650504846338f2e6067e48b0dc873b
Daw-Ran Liou <hi@dawranliou.com>
2024-02-29 15:20:49 -0800

    Sync packages 2024-02-29

---
+++ elpa/cape-20240226.558/cape-pkg.el
@@ -0,0 +1,15 @@
+(define-package "cape" "20240226.558" "Completion At Point Extensions"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "7353489c38622b1f1698ecfcc8d19ec7bfd71e1b" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("abbrev" "convenience" "matching" "completion" "text")
+  :url "https://github.com/minad/cape")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/jinx-20240229.1506/jinx-pkg.el
@@ -0,0 +1,15 @@
+(define-package "jinx" "20240229.1506" "Enchanted Spell Checker"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "8d71d54a897f7992e84d59493147eb9904824afd" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("convenience" "text")
+  :url "https://github.com/minad/jinx")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
