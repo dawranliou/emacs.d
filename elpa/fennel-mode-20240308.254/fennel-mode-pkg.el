d737ea62caa5a785a78743715dcfb75316413a17
Daw-Ran Liou <hi@dawranliou.com>
2024-03-08 16:00:06 -0800

    Sync packages 2024-03-08

---
+++ elpa/fennel-mode-20240308.254/fennel-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "fennel-mode" "20240308.254" "A major-mode for editing Fennel code"
+  '((emacs "26.1"))
+  :commit "6c0e6fe92950001d91b804445c2103f2362c5023" :authors
+  '(("Phil Hagelberg"))
+  :maintainers
+  '(("Phil Hagelberg"))
+  :maintainer
+  '("Phil Hagelberg")
+  :keywords
+  '("languages" "tools")
+  :url "https://git.sr.ht/~technomancy/fennel-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
