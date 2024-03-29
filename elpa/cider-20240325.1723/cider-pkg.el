be692e0b5ff3fc7ccfa3b955a9b90bcb90ca7e16
Daw-Ran Liou <hi@dawranliou.com>
2024-03-29 14:51:57 -0700

    Sync packages 2024-03-29

---
+++ elpa/cider-20240325.1723/cider-pkg.el
@@ -0,0 +1,26 @@
+(define-package "cider" "20240325.1723" "Clojure Interactive Development Environment that Rocks"
+  '((emacs "26")
+    (clojure-mode "5.18.1")
+    (parseedn "1.2.1")
+    (queue "0.2")
+    (spinner "1.7")
+    (seq "2.22")
+    (sesman "0.3.2")
+    (transient "0.4.1"))
+  :commit "f24060eb8be8084d318337873df1f9b5c3dd58e3" :authors
+  '(("Tim King" . "kingtim@gmail.com")
+    ("Phil Hagelberg" . "technomancy@gmail.com")
+    ("Bozhidar Batsov" . "bozhidar@batsov.dev")
+    ("Artur Malabarba" . "bruce.connor.am@gmail.com")
+    ("Hugo Duncan" . "hugo@hugoduncan.org")
+    ("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainers
+  '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
+  :maintainer
+  '("Bozhidar Batsov" . "bozhidar@batsov.dev")
+  :keywords
+  '("languages" "clojure" "cider")
+  :url "https://www.github.com/clojure-emacs/cider")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/dockerfile-mode-20240318.24/dockerfile-mode-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "dockerfile-mode" "20240318.24" "Major mode for editing Docker's Dockerfiles"
-  '((emacs "24")
-    (s "1.2.0"))
-  :commit "f6196726342b44081933597a343805db6366e7ac" :keywords
-  '("docker" "languages" "processes" "tools")
-  :url "https://github.com/spotify/dockerfile-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
