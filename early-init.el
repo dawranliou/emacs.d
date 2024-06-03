f442968b92f103135cd257818bdc027ac080ae88
Daw-Ran Liou <hi@dawranliou.com>
2024-06-03 07:31:26 -0700

    Suppress compilation warnings

--- early-init.el
+++ early-init.el
@@ -41,6 +41,11 @@
      (when (file-exists-p private-file)
        (load-file private-file)))))
 
+;; Supress warnings
+(setq byte-compile-warnings '(not obsolete)
+      warning-suppress-log-types '((comp) (bytecomp))
+      native-comp-async-report-warnings-errors 'silent)
+
 ;;; - UI
 
 (setq-default default-frame-alist
