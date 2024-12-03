deb86271983ecfb6cd6967016544f8a9fbc036b2
Daw-Ran Liou <hi@dawranliou.com>
2024-12-02 16:18:05 -0800

    Bind C-g to #'keyboard-quit-dwim
    Courtesy to Prot

--- init.el
+++ init.el
@@ -410,6 +410,7 @@ https://macowners.club/posts/custom-functions-4-ui/"
 ;;; Keybindings
 
 (keymap-global-set "<f5>" #'eshell-toggle)
+(keymap-global-set "C-g" #'keyboard-quit-dwim)
 (keymap-global-set "C-x C-b" #'ibuffer)
 (keymap-global-set "C-x k" #'kill-this-buffer)
 (keymap-global-set "C-x j" #'duplicate-dwim)
@@ -456,6 +457,31 @@ https://macowners.club/posts/custom-functions-4-ui/"
   "I can never remember the name of the function")
 
 ;;; Functions
+
+;; https://protesilaos.com/codelog/2024-11-28-basic-emacs-configuration/
+(defun keyboard-quit-dwim ()
+  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'."
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
 
 (defun maybe-split-window (&rest r)
   "Split window sensibly if there's only one window."
